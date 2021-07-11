0487098dd789dd82ab3549789ecf14905bb4da83
Pushp Singh <pushp_singh@condenast.com>
2021-07-11 22:57:44 +0530

    added boilerplate code for booking destination

---
+++ src/app/controller/booking-controller.js
@@ -0,0 +1,35 @@
+// All controller logic for CRUD operation will stay here
+
+const BookingModel = require('../model/booking-model');
+
+// Create and Save a new Booking
+exports.create = (req, res) => {
+    
+    // validate request body
+
+    // Creaate Booking
+
+    // Save Booking in Database
+ 
+};
+
+
+// Retrieve and return all Booking from the database.
+exports.findAll = (req, res) => {
+   // TODO from below
+};
+
+// Find a single Booking with a BookingId
+exports.findOne = (req, res) => {
+    // TODO from below
+};
+
+// Update a Booking identified by the BookingId in the request
+exports.update = (req, res) => {
+    // TODO from below
+};
+
+// Delete a Booking with the specified BookingId in the request
+exports.delete = (req, res) => {
+    // TODO from below
+};
---
+++ src/app/routes/index.js
@@ -0,0 +1,16 @@
+// all routes HTTP method (GET/PUT/POST/DELETE) will be present here
+
+module.exports = (app) => {
+    // import your existing Booking controller here, which contains method for all CRUD operations
+
+    // APi endpoint to create a new Booking item
+
+    // API endpoint to get all Booking items
+
+    // API endpoint to get single Booking item
+
+    // API endpoint to update Booking with BookingId
+
+    // API endpoint to delete Booking with BookingId
+
+};
